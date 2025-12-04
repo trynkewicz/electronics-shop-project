bcfab5d2692bcd14099019db8e545e6858c2b8ac
Evgenii Surmin <surmin1999@mail.ru>
2025-12-04 17:15:19 +0300

    3) app item, test_item

--- tests/test_item.py
+++ tests/test_item.py
@@ -43,3 +43,11 @@ def test_name_setter():
 
     item.name = "СуперСмартфон"
     assert item.name == "СуперСмарт"  # первые 10 символов
+
+def test_repr():
+    item1 = Item("Смартфон", 10000, 20)
+    assert repr(item1) == "Item('Смартфон', 10000, 20)"
+
+def test_str():
+    item1 = Item("Смартфон", 10000, 20)
+    assert str(item1) == 'Смартфон'
